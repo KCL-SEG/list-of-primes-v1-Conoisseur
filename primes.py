46b1287e05c8a2fbcc63ff19007095eacdf395d4
Conoisseur <97054370+Conoisseur@users.noreply.github.com>
2022-10-10 21:24:45 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,19 @@
 
 def primes(number_of_primes):
     list = []
+    list.append(2)
+
+    for i in range(number_of_primes-1):
+        numberToCheck = list[-1] + 1
+        while not isPrime(numberToCheck):
+            numberToCheck += 1
+        list.append(numberToCheck)
+
     return list
+
+def isPrime(number):
+    for i in range(2,number):
+        if number % i == 0:
+            return False
+    return True
+
